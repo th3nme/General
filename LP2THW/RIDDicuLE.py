329d4269a5b42cb526bc05a7cc176359d3841bc5
Nick Ephraims <nick.ephraims@gmail.com>
2017-11-07 17:37:27 +0100

    Riddicule updates

--- LP2THW/RIDDicuLE.py
+++ LP2THW/RIDDicuLE.py
@@ -1,4 +1,4 @@
-# RIDDicuLE
+"""RIDDicuLE The Game"""
 # Author: Nick Ephraims
 
 from sys import exit
@@ -73,12 +73,10 @@ def random_riddle():
         "Which word is always spelt incorrectly?":
             "incorrectly",
     }
-    
     # Give the player 3 chances to give a correct answer
     chances_remaining = 3
     correct_answer_given = False
-
-    # Ask random riddle until correct answer or 3 incorrect answers are given 
+    # Ask random riddle until correct answer or 3 incorrect answers are given
     while chances_remaining > 0 and correct_answer_given == False:
         # Get a random riddle and answer from riddles{}
         question = random.choice(list(riddles.keys()))
@@ -97,54 +95,46 @@ def random_riddle():
             correct_answer_given = True
             print "The creature accepts your answer as correct."
             print "You are allowed to live a little bit longer."
-    # If you player runs out of chances they die or if not they win and move to the next level
     if chances_remaining == 0:
-        return 'death'
-    else:
-        pass
+        deaths = [
+            "The creature deems your answers to be insufficient and decides to"
+            "rip your head clean off. Oh No! It appears that you are dead.",
+            "The creature ponders our response briefly but ultimately decides"
+            "that it doesn't like your answers and fly kicks you right in the face."
+            "Your head is parted from your shoulders killing you instantly",
+            "The creature licks its lips and after a brief moment decides that your"
+            "answers are wrong and races towards you and a frightening pace. It tears"
+            "your body limb from limb before chewing your flesh leaving nothing"
+            "but clean bones behind. Guess that means youe dead!"
+        ]
+        print deaths[randint(0, len(deaths)-1)]
+        exit()
 
 class Level(object):
-    
+    """Parent class of game levels (not used yet)"""
     def enter(self):
+        """Nothing"""
         print "This class is not yet configured."
         exit(1)
 
 class Engine(object):
-    
+    """Engine controls which class is the current level and runs it accordingly"""
     def __init__(self, level_map):
         self.level_map = level_map
 
     def play(self):
+        """Runs the current level"""
         current_level = self.level_map.opening_level()
         last_level = self.level_map.next_level('rooftop')
 
         while current_level != last_level:
             next_level_name = current_level.enter()
             current_level = self.level_map.next_level(next_level_name)
-        
         # Be sure to print out the last level
         current_level.enter()
 
-class Death(Level):
-    
-    quips = [
-        "The creature deems your answers to be insufficient and decides to" 
-        "rip your head clean off. Oh No! It appears that you are dead.",
-        "The creature ponders our response briefly but ultimately decides"
-        "that it doesn't like your answers and fly kicks you right in the face."
-        "Your head is parted from your shoulders killing you instantly",
-        "The creature licks its lips and after a brief moment decides that your"
-        "answers are wrong and races towards you and a frightening pace. It tears"
-        "your body limb from limb before chewing your flesh leaving nothing"
-        "but clean bones behind. Guess that means youe dead!"
-    ]
-
-    def enter(self):
-        print Death.quips[randint(0, len(self.quips)-1)]
-        exit(1)
-
 class Basement(Level):
-    
+    """First level"""
     def enter(self):
         print "=" * 60
         print "You wake up in a basement, head throbbing and unable to remember"
@@ -156,7 +146,7 @@ class Basement(Level):
         print "As you approach they start rambling about riddles, short answers and"
         print "three chances... perhaps this will make sense later. You notice that"
         print "the only way out is a staircase heading upstairs so you decide to"
-        print "see if there is a way out upstairs."
+        print "see if there is another way out."
         print "=" * 60
         raw_input("'Enter'> ")
         return 'first_floor'
@@ -165,61 +155,90 @@ class FirstFloor(Level):
     """Undead zombie level"""
     def enter(self):
         print "=" * 60
+        print "You enter the room and notice the doors leading outside are chained shut."
+        print "You hear a scraping noise and turn around. A horrible zombie like creature"
+        print "hobbles towards you. Its skin is grey and rotting showing bone and flesh"
+        print "in some places. Its pupil-less eyes glow with a dim white luminescence."
+        print "In a raspy voice the undead creature tells you that you will be allowed to"
+        print "pass if you correctly answer a riddle in 3 attempts. If you fail it will"
+        print "bite you spreading the disease and you too will become undead."
+        print "=" * 60
+        raw_input("'Enter'> ")
         random_riddle()
+        print "The undead zombie steps aside and you take the stairs up to the next floor."
         return 'second_floor'
 
 class SecondFloor(Level):
-    
+    """Giant snake level"""
     def enter(self):
-        print "second floor"
+        print "Second floor"
+        random_riddle()
+        return 'third_floor'
 
 class ThirdFloor(Level):
-    
+    """Medusa level"""
     def enter(self):
-        pass
+        print "Third floor"
+        random_riddle()
+        return 'fourth_floor'
 
 class FourthFloor(Level):
-    
+    """Minotaur level"""
     def enter(self):
-        pass
+        print "Fourth floor"
+        random_riddle()
+        return 'fifth_floor'
 
 class FifthFloor(Level):
-    
+    """Giant spider level"""
     def enter(self):
-        pass
+        print "Fitfh floor"
+        random_riddle()
+        return 'sixth_floor'
 
 class SixthFloor(Level):
-    
+    """Demon level"""
     def enter(self):
-        pass
+        print "Sixth floor"
+        random_riddle()
+        return 'seventh_floor'
 
 class SeventhFloor(Level):
-    
+    """Dracula level"""
     def enter(self):
-        pass
+        print "Seventh floor"
+        random_riddle()
+        return 'eighth_floor'
 
 class EighthFloor(Level):
-    
+    """Giant Squid level"""
     def enter(self):
-        pass
+        print "Eighth floor"
+        random_riddle()
+        return 'ninth_floor'
 
 class NinthFloor(Level):
-    
+    """T-Rex level"""
     def enter(self):
-        pass
+        print "Ninth floor"
+        random_riddle()
+        return 'tenth_floor'
 
 class TenthFloor(Level):
-    
+    """Godzilla level"""
     def enter(self):
-        pass
+        print "Tenth floor"
+        random_riddle()
+        return 'rooftop'
 
 class Rooftop(Level):
-    
+    """Escape Helicopter level"""
     def enter(self):
-        pass
+        print "You win"
+        exit(0)
 
 class Map(object):
-    
+    """Level map"""
     levels = {
         'basement': Basement(),
         'first_floor': FirstFloor(),
@@ -233,9 +252,7 @@ class Map(object):
         'ninth_floor': NinthFloor(),
         'tenth_floor': TenthFloor(),
         'rooftop': Rooftop(),
-        'death': Death(),
     }
-    
     def __init__(self, start_level):
         self.start_level = start_level
 
