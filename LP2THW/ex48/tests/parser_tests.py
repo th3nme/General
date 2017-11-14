42aa7fd4eb09b6800ba1c7f305bd81db2caefb1f
Nick Ephraims <nick.ephraims@gmail.com>
2017-11-14 17:20:00 +0100

    ex49

---
+++ LP2THW/ex48/tests/parser_tests.py
@@ -0,0 +1,23 @@
+from nose.tools import *
+from ex48 import parser
+
+def test_sentence():
+    pass
+
+def test_peek():
+    pass
+
+def test_match():
+    pass
+
+def test_parse_verb():
+    pass
+
+def test_parse_object():
+    pass
+
+def test_parse_subject():
+    pass
+
+def test_parse_sentence():
+    pass
