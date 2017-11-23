551c9bd69a0e01de3287754d30e91eef6a5321c6
Nick Ephraims <nick.ephraims@gmail.com>
2017-11-23 15:25:30 +0100

    ex5.py

---
+++ LP3THW/ex5.py
@@ -0,0 +1,17 @@
+my_name = 'Nick Ephraims'
+my_age = 27 # not a lie
+my_height = 192 # centimetres
+my_weight = 95 # kilos
+my_eyes = 'Green'
+my_teeth = 'White'
+my_hair = 'Brown'
+
+print(f"Let's talk about {my_name}.")
+print(f"He's {my_height} centimetres tall.")
+print(f"He's {my_weight} kilos heavy.")
+print("Actually, that's not too heavy")
+print(f"He's got {my_eyes} eyes and {my_hair} hair.")
+print(f"His teeth are usually {my_teeth} depending on the coffee.")
+
+total = my_age + my_height + my_weight
+print(f"If I add {my_age}, {my_height}, and {my_weight} I get {total}.")
