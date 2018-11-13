0df6dc46ce02104a82d88328cd534bce921b70e9
Nick Ephraims <nick.ephraims@Certara.com>
2018-11-13 15:29:50 +0100

    ex5

--- LP3THW/ex5.py
+++ LP3THW/ex5.py
@@ -1,19 +1,19 @@
-my_name = 'Nick Ephraims'
-my_age = 28
-my_height = 192
-my_weight = 95
-my_weight_pounds = round(my_weight * 2.2)
-my_eyes = 'Green'
-my_teeth = 'White'
-my_hair = 'Brown'
+name = 'Nick Ephraims'
+age = 28
+height = 192
+weight = 95
+weight_pounds = round(weight * 2.2)
+eyes = 'Green'
+teeth = 'White'
+hair = 'Brown'
 
-print(f"Let's talk about {my_name}.")
-print(f"He's {my_height} centimeters tall.")
-print(f"He weighs {my_weight} kilograms or {my_weight_pounds} pounds.")
+print(f"Let's talk about {name}.")
+print(f"He's {height} centimeters tall.")
+print(f"He weighs {weight} kilograms or {weight_pounds} pounds.")
 print("Actually that's not too heavy")
-print(f"He's got {my_eyes} eyes and {my_hair} hair.")
-print(f"His teeth are usually {my_teeth} depending on the coffee.")
+print(f"He's got {eyes} eyes and {hair} hair.")
+print(f"His teeth are usually {teeth} depending on the coffee.")
 
 # This line is tricky, try to get it exactly right.
-total = my_age + my_height + my_weight
-print(f"If I add his age: {my_age}, height: {my_height} and weight: {my_weight} I get a total of: {total}")
+total = age + height + weight
+print(f"If I add his age: {age}, height: {height} and weight: {weight} I get a total of: {total}")
