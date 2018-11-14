4034b08a0bdfd36d46652c7ee99f91d1d81147da
Nick Ephraims <nick.ephraims@Certara.com>
2018-11-14 17:56:36 +0100

    ex12 and ex11 update

--- LP3THW/ex11.py
+++ LP3THW/ex11.py
@@ -1,3 +1,4 @@
+# Ask the user for their age, height and weight
 print("How many years old are you?", end=' ')
 age = input()
 print("How many centimeters tall are you?", end=' ')
@@ -5,4 +6,5 @@ height = input()
 print("How many kilos do you weigh?", end=' ')
 weight = input()
 
+# Print the user's responses back to them
 print(f"So, you're {age} years old, {height} centimeters tall and weigh {weight} kilos.")
