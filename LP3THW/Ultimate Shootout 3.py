6534cbc89bef5faa060fcb2921b50e437f67a624
Nick Ephraims <nick.ephraims@Certara.com>
2018-12-14 16:52:49 +0100

    Shootout 3

---
+++ LP3THW/Ultimate Shootout 3.py
@@ -0,0 +1,42 @@
+import random
+from sys import exit
+# Ultimate Penalty Shootout 3
+# Author: Nick Ephraims
+
+# We have a penalty spot kick. The player selects from 3 options and the goalkeeper selects randomly.
+# If there is a match the shot is saved otherwise a goal is scored
+
+# Function for penalty shot randomizer / score keeper
+# second function for goal keeper AI shot randomizer
+# seperate functions for goal scored, goal missed, goal saved, goal conceded. 
+# invalid response slipped voer and booted ball into the stands
+# if x shots remaining and score difference is + or - certain amount then exit
+
+print("=" * 80)
+print("You are playing in the Football World Cup Final! Extra time is over but the scores are still tied.")
+print("That means the match will be decided in a penalty shootout! Both teams line up along the half way line.")
+print("The referee blows his whistle, its a PENALTY! The crowd roars continuously.")
+print("The atmosphere is reaching fever pitch! The crowd is going ballistic. It all comes down to this")
+print("=" * 80)
+
+def penalty_kick():
+    print("=" * 80)
+
+
+def penalty_defend():
+    print("=" * 80)
+
+
+def goal_scored():
+    print("=" * 80)
+
+def goal_scored_():
+    print("=" * 80)
+
+def goal_saved():
+    print("=" * 80)
+
+def error():
+    print("=" * 80)
+
+penalty_kick()
